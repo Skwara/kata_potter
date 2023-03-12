ead90c613e088737ce00e283fbe4a0e0dd19ed7b
Skwara <skwara0@gmail.com>
2023-03-12 23:56:27 +0100

    Unfinished new approach

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import pytest
 
 
 class PriceCalculator:
-    DISCOUNTS = {1: 0, 2: 0.05, 3: 0.1, 4: 0.2, 5: 0.25}  # 6: 1
+    DISCOUNTS = {1: 0, 2: 0.05, 3: 0.1, 4: 0.2, 5: 0.25, 6: 0.4}
     BOOK_PRICE = 8
 
-    def __init__(self, basket):
+    def __init__(self, basket: [str]):
         self.basket = basket
 
     def calculate_price(self):
@@ -13,25 +13,33 @@ class PriceCalculator:
 
     def create_discount_sets(self):
         discount_sets = []
-        for book in self.basket:
-            best_set = []
-            for book_set in discount_sets:
-                if book not in book_set:
-                    if PriceCalculator.book_set_score(best_set + [book]) < PriceCalculator.book_set_score(book_set + [book]):
-                        best_set = book_set
-                    elif PriceCalculator.book_set_score(best_set + [book]) == PriceCalculator.book_set_score(book_set + [book]):
-                        if len(best_set) > len(book_set):
-                            best_set = book_set
-            if not best_set:
-                discount_sets.append(best_set)
-            best_set.append(book)
+        ordered_discounts = PriceCalculator.order_discounts_by_score()
+
+        score = PriceCalculator.discount_score(ordered_discounts[0])
+        for discount in ordered_discounts:
+            discount_score = PriceCalculator.discount_score(discount)
+            assert discount_score <= score
+            score = discount_score
+            while self.basket:
+                unique_set = set(self.basket)  # Consider created sets, but only with score equal or lower? Not really
+                if discount[0] > len(unique_set):
+                    break
+                diff = len(unique_set) - discount[0]
+                unique_set = list(unique_set)[diff:]
+                discount_sets.append(unique_set)
+                for book in unique_set:
+                    self.basket.remove(book)
         return discount_sets
 
     @staticmethod
-    def book_set_score(book_set):
-        if not book_set:
+    def order_discounts_by_score() -> [()]:
+        return sorted(PriceCalculator.DISCOUNTS.items(), key=lambda discount: PriceCalculator.discount_score(discount), reverse=True)
+
+    @staticmethod
+    def discount_score(discount):
+        if not discount[0]:
             return -1
-        return PriceCalculator.DISCOUNTS[len(book_set)] / len(book_set)
+        return discount[1] / discount[0]
 
     @staticmethod
     def calc_set_price(book_set):
@@ -65,3 +73,12 @@ def test_non_greedy_discount():
 def test_double_non_greedy_discount():
     assert PriceCalculator(["a", "b", "c", "d", "a", "b", "c", "a", "b", "c", "e", "d"]).calculate_price() == 3 * 4 * 8 * 0.8
     assert PriceCalculator(["a", "b", "c", "d", "a", "b", "c", "a", "b", "c", "e", "d", "a"]).calculate_price() == 3 * 4 * 8 * 0.8 + 8
+
+
+def test_discount_score_increase_after_equal_scores():
+    assert PriceCalculator(["a", "b", "c", "d", "a", "b", "c", "e", "f"]).calculate_price() == 6 * 8 * 0.6 + 3 * 8 * 0.9
+
+
+def test_method_order_discounts_by_score_simple():
+    print(PriceCalculator.order_discounts_by_score())
+    assert PriceCalculator.order_discounts_by_score() == [(6, 0.4), (4, 0.2), (5, 0.25), (3, 0.1), (2, 0.05), (1, 0)]
